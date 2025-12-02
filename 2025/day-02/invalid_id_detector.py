278596f7ee43484857b3ca4560b9e210e8d88214
udaynmenon <udaynmenon@gmail.com>
2025-12-02 14:36:57 +0100

    day 2 part 2

--- 2025/day-02/invalid_id_detector.py
+++ 2025/day-02/invalid_id_detector.py
@@ -1,3 +1,4 @@
+import re
 from typing import List
 from pathlib import Path
 from dataclasses import dataclass
@@ -12,17 +13,19 @@ class Range:
     end: str
 
 
+RE_REPEAT = re.compile(r"^(\d+)\1+$")
+
+
 # Reading turns from file
 def read_id_ranges(file_path: Path) -> List[Range]:
     id_ranges = list()
     with open(file_path, "r") as file:
-        ranges = file.readline().split(",")
+        content = file.read().strip()
+        raw_ranges = content.split(",")
 
-        for id_range in ranges:
-            split_location = id_range.find("-")
-            start_val = id_range[0:split_location]
-            end_val = id_range[split_location + 1 :]
-            id_ranges.append(Range(start_val, end_val))
+        for id_range in raw_ranges:
+            start, end = id_range.split("-")
+            id_ranges.append(Range(start, end))
     return id_ranges
 
 
@@ -40,15 +43,19 @@ def is_valid(value: str) -> bool:
         return True
 
 
+def is_valid_advanced(value: int) -> bool:
+    return False if RE_REPEAT.match(str(value)) else True
+
+
 def invalid_sum(ranges: List[Range]) -> int:
     sum = 0
     for id_range in ranges:
         for i in range(int(id_range.start), int(id_range.end) + 1):
-            if not is_valid(str(i)):
+            if not is_valid_advanced(i):
                 sum += i
 
     return sum
 
 
 print(invalid_sum(read_id_ranges(SOURCE)))
-# print(invalid_sum([Range("11", "22")]))
+# print(invalid_sum([Range("95", "115")]))
