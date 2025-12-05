4f64cd66bf6d46518233819f35bda7b09128ed91
udaynmenon <udaynmenon@gmail.com>
2025-12-05 09:49:43 +0100

    day 5 cleanup

--- 2025/day-05/ingredients.py
+++ 2025/day-05/ingredients.py
@@ -1,12 +1,12 @@
 # Link to puzzle: https://adventofcode.com/2025/day/5
 
-from typing import List, Final
+from typing import List, Tuple, Set
 from pathlib import Path
 from dataclasses import dataclass
 
 # --- Configuration & Constants ---
 PROJECT_DIR = Path(__file__).parent
-SOURCE = PROJECT_DIR / "test.txt"
+SOURCE = PROJECT_DIR / "input.txt"
 
 
 @dataclass
@@ -15,48 +15,49 @@ class Range:
     end: int
 
 
-id_ranges = []
-
-
-def parse_ingredients(file_path: Path) -> set:
-    available_ingredients = set()
-
-    ingredients_mode = False
+def parse_ingredients(file_path: Path) -> Tuple[List[Range], Set[int]]:
+    """
+    Parses the input file into two datasets:
+    1. A list of fresh ingredient ID ranges.
+    2. A set of specific available ingredient IDs to check.
+    """
+    raw_id_ranges: List[Range] = []
+    available_ingredients: Set[int] = set()
+    parse_range_mode = True
 
     with open(file_path, "r", encoding="utf-8") as file:
         for line in file:
             clean_line = line.strip()
 
             if not clean_line:
-                ingredients_mode = True
+                parse_range_mode = False
                 continue
 
-            if not ingredients_mode:
+            if parse_range_mode:
                 raw_ranges = clean_line.split(",")
 
                 for raw_range in raw_ranges:
                     start_str, end_str = raw_range.split("-")
-                    id_ranges.append(Range(int(start_str), int(end_str)))
+                    raw_id_ranges.append(Range(int(start_str), int(end_str)))
 
             else:
                 available_ingredients.add(int(clean_line))
 
-    # print(id_ranges)
-    # print("tasdtsadt")
-    # print(available_ingredients)
-
-    return available_ingredients
+    return raw_id_ranges, available_ingredients
 
 
-def in_range(ingredient: int) -> bool:
+def is_fresh(ingredient_id: int, id_ranges: List[Range]) -> bool:
     for r in id_ranges:
-        if ingredient >= r.start and ingredient <= r.end:
+        if r.start <= ingredient_id <= r.end:
             return True
+
+        if r.start > ingredient_id:
+            return False
     return False
 
 
-def merge_overlapping_ranges():
-    sorted_ranges = sorted(id_ranges, key=lambda r: r.start)
+def merge_overlapping_ranges(raw_id_range: List[Range]) -> List[Range]:
+    sorted_ranges = sorted(raw_id_range, key=lambda r: r.start)
 
     merged = list()
 
@@ -75,11 +76,11 @@ def merge_overlapping_ranges():
     return merged
 
 
-def solve_part_one(ingredients: set) -> int:
+def solve_part_one(ingredients: set, id_ranges: List[Range]) -> int:
     total_fresh = 0
 
     for ingredient in ingredients:
-        if in_range(ingredient):
+        if is_fresh(ingredient, id_ranges):
             total_fresh += 1
 
     return total_fresh
@@ -99,15 +100,17 @@ def main():
     """
     Orchestrates the solution.
     """
-    available_ingredients = parse_ingredients(SOURCE)
-    if not available_ingredients:
+    raw_id_ranges, available_ingredients = parse_ingredients(SOURCE)
+    if not raw_id_ranges and available_ingredients:
         return
 
+    merged_id_ranges = merge_overlapping_ranges(raw_id_ranges)
+
     # Part 1
-    print(f"Solution 1: {solve_part_one(available_ingredients)}")
+    print(f"Solution 1: {solve_part_one(available_ingredients, merged_id_ranges)}")
 
     # Part 2
-    print(f"Solution 2: {solve_part_two(merge_overlapping_ranges())}")
+    print(f"Solution 2: {solve_part_two(merged_id_ranges)}")
 
 
 if __name__ == "__main__":
